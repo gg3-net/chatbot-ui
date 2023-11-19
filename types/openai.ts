3f74dd48410b24676873506ac8d5700ad1f924bb
Georgi Georgiev <310867+chutzimir@users.noreply.github.com>
2023-11-19 22:38:45 +0900

    Typo?

--- types/openai.ts
+++ types/openai.ts
@@ -9,13 +9,13 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
+  GPT_3_5_TURBO = 'gpt-3.5-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_128K = 'gpt-4-1106-preview'
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_TURBO;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
@@ -24,8 +24,8 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
+  [OpenAIModelID.GPT_3_5_TURBO]: {
+    id: OpenAIModelID.GPT_3_5_TURBO,
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
